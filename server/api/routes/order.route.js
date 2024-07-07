cb58403b30dba9dce204343959c4268a2f04e6bc
haileiyesus-29 <haileiyesus29@gmail.com>
2024-07-07 22:38:50 +0300

    replace  old code

--- server/api/routes/order.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { orderProduct, getOrders } = require('../controllers/order.controller')
-const authenticate = require('../middlewares/authenticate')
-
-router.get('/', authenticate, getOrders)
-router.post('/', authenticate, orderProduct)
-
-module.exports = router
--- server/api/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const validateUserCrediential = require('../validations/validateUserCredential')
-const { createUser } = require('../controllers/user.controller')
-
-router.post('/create', validateUserCrediential, createUser)
-
-module.exports = router
