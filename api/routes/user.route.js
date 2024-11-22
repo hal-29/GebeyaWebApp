4386dbfa6033676f08d9e2d6ac0629845137eda1
haileiyesus-29 <haileiyesus29@gmail.com>
2024-11-22 16:27:46 +0300

    initial cmmit

--- api/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getUserInfo } = require('../controllers/user.controller')
-const authenticate = require('../middlewares/authenticate')
-const router = express.Router()
-
-router.get('/', authenticate, getUserInfo)
-
-module.exports = router
--- ui/src/pages/Account.jsx
+++
@@ -1,6 +0,0 @@
-function Account() {
-   return (
-      <section className='top-[calc(100svh_-10rem)] absolute bg-gray-100 w-[40rem]'></section>
-   )
-}
-export default Account
